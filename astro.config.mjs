7ddfba8cc1984a08a5565e1749e39d77ab746d33
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-10-28 18:51:12 +0530

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   server: {
     port: 3000
   },
---
+++ src/lib/api/fetchFeeds.ts
@@ -0,0 +1,62 @@
+import { connectDatabase } from "../../data/connect"
+import { SQL_QUERY_BASE_PATH } from "../../lib/constants"
+import { PostgresError } from "postgres"
+
+export const fetchFeeds = async (request: Record<string, any>) => {
+    const client = connectDatabase()
+    const queryParams = new URLSearchParams(request.url.split("?")[1])
+    const feedId = queryParams.get('id');
+    const file = feedId ? `getSingleFeed` : `getListOfFeeds`
+
+    try {
+        const queryResult = feedId 
+            ? await client.file<[{ id: string, url: string }]>(`${SQL_QUERY_BASE_PATH}/${file}.sql`, [`${feedId}`]) 
+            : await client.file<[{ id: string, url: string }]>(`${SQL_QUERY_BASE_PATH}/${file}.sql`);
+    
+        await client.end();
+        
+        return (
+            JSON.stringify({
+                message: "Success",
+                status: 200,
+                method: request.method,
+                data: queryResult
+            })
+        )
+    } catch (error) {
+        await client.end()
+
+        if(error instanceof PostgresError) {
+            throw Error (
+                JSON.stringify({
+                    method: request.method,
+                    code: error.code,
+                    cause: error.cause,
+                    message: error.message,
+                    hint: error.hint,
+                    where: error.where,
+                    query: error.query,
+                    status: 400,
+                })
+            )
+
+            
+        } else if(error instanceof Error) {
+            throw Error (
+                JSON.stringify({
+                    method: request.method,
+                    message: error.message,
+                    status: 500,
+                })
+            )
+        } else {
+            throw new Error (
+                JSON.stringify({
+                    method: request.method,
+                    message: "Someting went wrong",
+                    status: 500,
+                })
+            )
+        }
+    }
+}
