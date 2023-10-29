7250b8eeec40d55261f3d97f5401a2f141c78c51
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-10-29 16:44:40 +0530

    fixing hhook

--- netlify/functions/scheduledBuild.ts
+++ netlify/functions/scheduledBuild.ts
@@ -6,14 +6,22 @@ const BUILD_HOOK = process.env.BUILD_HOOK as unknown as URL
 /**
  * ? every day at 00:00
  */
-export const handler = schedule('0 0 * * *', async () => {
-    const res = await fetch(BUILD_HOOK, {
-        method: "POST"
-    })
-    const data = await res.json()
-    console.log(data)
+export const handler = schedule('*/2 * * * *', async () => {
+    try {
+        const res = await fetch(BUILD_HOOK, {
+            method: "POST"
+        })
+        // const data = await res.json()
+        console.log(res)
+        return {
+            statusCode: 200
+        }
+    } catch (error) {
+        console.log(error)
 
-    return {
-        statusCode: 200
+        return {
+            statusCode: 500
+        }
     }
+
 })
